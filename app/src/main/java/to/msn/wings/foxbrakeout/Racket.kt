4bcec916c382e66d0c3c9c61b3caad2766458a3e
kaga1minn0nnn4 <toida2003@gmail.com>
2023-09-01 16:02:48 +0900

    add move range in Racket

--- app/src/main/java/to/msn/wings/foxbrakeout/Racket.kt
+++ app/src/main/java/to/msn/wings/foxbrakeout/Racket.kt
@@ -6,6 +6,7 @@ import android.graphics.Paint
 import android.graphics.Point
 
 class Racket(private val centerX: Float, private val size: Point): RectangleObject(centerX, size.y - 200f) {
+
     override val p: Paint = Paint().apply {
         color = Color.BLACK
     }
@@ -14,6 +15,12 @@ class Racket(private val centerX: Float, private val size: Point): RectangleObje
     override val width: Float = 200f
 
     fun move(newCenterX: Float): Racket {
+        val min = width / 2
+        val max = size.x - width / 2
+
+        if (newCenterX < min) return Racket(min, size)
+        if (newCenterX > max) return Racket(max, size)
+
         return Racket(newCenterX, size)
     }
 
